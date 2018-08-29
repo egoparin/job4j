f59d3f656fb0a5a5c933e12ba239d0fe2e51773b
Oparin Egor <mailto:egoparin@gmail.com>
2018-08-29 08:47:56 +0300

    correct test

--- chapter_001/src/main/java/ru/job4j/condition/Point.java
+++ chapter_001/src/main/java/ru/job4j/condition/Point.java
@@ -32,16 +32,4 @@ public class Point {
 
         return Math.sqrt(Math.pow(x2 - x1, 2) + Math.sqrt(Math.pow(y2 - y1, 2)));
     }
-
-    public static void main(String[] args) {
-        Point a = new Point(0, 1);
-        Point b = new Point(2, 3);
-
-        System.out.println("x1 = " + a.x);
-        System.out.println("y1 = " + a.y);
-        System.out.println("x2 = " + b.x);
-        System.out.println("y1 = " + b.y);
-
-        System.out.println("Distance between points: " + a.distanceTo(b));
-    }
 }
--- chapter_001/src/test/java/ru/job4j/condition/PointTest.java
+++ chapter_001/src/test/java/ru/job4j/condition/PointTest.java
@@ -1,4 +1,24 @@
 package ru.job4j.condition;
 
+import org.junit.Test;
+
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.assertThat;
+
+/**
+ * Test.
+ *
+ * @author Egor Oparin (egoparin@gmail.com)
+ * @version $Id$
+ * @since 0.1
+ */
 public class PointTest {
+    Point a = new Point(0, 1);
+    Point b = new Point(2, 1);
+
+    @Test
+    public void when1Point01and2Point21then2() {
+        double result = a.distanceTo(b);
+        assertThat(result, is(2D));
+    }
 }
