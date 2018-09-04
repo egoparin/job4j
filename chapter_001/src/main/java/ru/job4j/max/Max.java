dff2db78b8ea6a448410d136a09d9a1f2ddd77a2
Oparin Egor <EgOparin@croc.ru>
2018-09-04 09:19:21 +0300

    Stupid bot

---
+++ chapter_001/src/main/java/ru/job4j/max/Max.java
@@ -0,0 +1,22 @@
+package ru.job4j.max;
+
+/**
+ * Max of two numbers
+ *
+ * @author Egor Oparin (mailto:egoparin@gmail.com)
+ * @version $Id$
+ * @since 0.1
+ */
+
+public class Max {
+    /**
+     * Answer of questions
+     *
+     * @param first
+     * @param second - input numbers.
+     * @return biggest num
+     */
+    public int maxNumber(int first, int second) {
+        return (first > second) ? first : (first == second) ? 0 : second;
+    }
+}
---
+++ chapter_001/src/test/java/ru/job4j/max/MaxTest.java
@@ -0,0 +1,36 @@
+package ru.job4j.max;
+
+import org.junit.Test;
+
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.assertThat;
+
+/**
+ * Test.
+ *
+ * @author Egor Oparin (egoparin@gmail.com)
+ * @version $Id$
+ * @since 0.1
+ */
+public class MaxTest {
+
+    Max max = new Max();
+
+    /**
+     * Test
+     */
+    @Test
+    public void when1and2Then2() {
+        assertThat(max.maxNumber(1, 2), is(2));
+    }
+
+    @Test
+    public void when5and3Then5() {
+        assertThat(max.maxNumber(5, 3), is(5));
+    }
+
+    @Test
+    public void when2and2Then0() {
+        assertThat(max.maxNumber(2, 2), is(0));
+    }
+}
