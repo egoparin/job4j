e3240f63e8326f82afab552b6ab5b8a941e7e7ff
Oparin Egor <EgOparin@croc.ru>
2018-12-11 08:24:14 +0300

    5.1.1 Итератор для 2х мерного массива

--- chapter_002/src/main/java/ru/job4j/collections_pro/itetator/IteratorMultDimenArray.java
+++ chapter_002/src/main/java/ru/job4j/collections_pro/itetator/IteratorMultDimenArray.java
@@ -10,21 +10,15 @@ public class IteratorMultDimenArray {
 
     private int indexRow = 0;
     private int indexColumn = 0;
-    private int lastIndexRow;
-    private int lastIndexColumn;
     private final int[][] values;
 
 
     public IteratorMultDimenArray(final int[][] values) {
         this.values = values;
-        this.lastIndexRow = values.length - 1;
-        for (int lastColumn = 0; lastColumn < values[values.length - 1].length; lastColumn++) {
-            lastIndexColumn = lastColumn;
-        }
     }
 
     public boolean hasNext() {
-        return !(lastIndexRow == indexRow && lastIndexColumn+1 == indexColumn);
+        return !(values.length - 1 == indexRow && values[values.length - 1].length == indexColumn);
     }
 
     public Object next() {
