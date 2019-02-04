02892eaca8b5c66186c163a73b4888c721f40856
Oparin Egor <EgOparin@croc.ru>
2019-02-04 10:10:49 +0300

    5.3.0 Создать метод delete для односвязного списка[#75375]

--- chapter_002/src/main/java/ru/job4j/collections/list/SimpleArrayList.java
+++ chapter_002/src/main/java/ru/job4j/collections/list/SimpleArrayList.java
@@ -1,4 +1,69 @@
 package ru.job4j.collections.list;
 
-public class SimpleArrayList {
+
+/**
+ * @author Egor Oparin (mailto:egoparin@gmail.com)
+ * @version $Id$
+ * @since 28.01.2019
+ */
+public class SimpleArrayList<E> {
+    private int size;
+    private Node<E> first;
+
+    /**
+     * Method add new data in list.
+     *
+     * @param data
+     */
+    public void add(E data) {
+        Node<E> newLink = new Node<>(data);
+        newLink.next = this.first;
+        this.first = newLink;
+        this.size++;
+    }
+
+    /**
+     * Method delete first element from collection.
+     */
+    public E delete() {
+        Node<E> result = this.first;
+        this.first = result.next;
+        size--;
+        return result.data;
+    }
+
+    /**
+     * Method to get element at index.
+     *
+     * @param index
+     */
+    public E get(int index) {
+        Node<E> result = this.first;
+        for (int i = 0; i < index; i++) {
+            result = result.next;
+        }
+        return result.data;
+    }
+
+    /**
+     * Method to get size collection.
+     */
+    public int getSize() {
+        return this.size;
+    }
+
+    /**
+     * Inner class for store data.
+     *
+     * @param <E>
+     */
+    private class Node<E> {
+        E data;
+        Node<E> next;
+
+        Node(E data) {
+            this.data = data;
+
+        }
+    }
 }
--- chapter_002/src/test/java/ru/job4j/collections/list/SimpleArrayListTest.java
+++ chapter_002/src/test/java/ru/job4j/collections/list/SimpleArrayListTest.java
@@ -1,4 +1,37 @@
 package ru.job4j.collections.list;
 
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.assertThat;
+
+/**
+ * @author Egor Oparin (mailto:egoparin@gmail.com)
+ * @version $Id$
+ * @since 28.01.2019
+ */
 public class SimpleArrayListTest {
+    private SimpleArrayList<Integer> simpleArrayList = new SimpleArrayList<Integer>();
+
+    @Before
+    @Test
+    public void whenAdd() {
+        simpleArrayList.add(2);
+        simpleArrayList.add(3);
+        simpleArrayList.add(4);
+
+        assertThat(simpleArrayList.get(1), is(3));
+        assertThat(simpleArrayList.get(0), is(4));
+    }
+
+    @Test
+    public void whenDelete() {
+        assertThat(simpleArrayList.delete(), is(4));
+    }
+
+    @Test
+    public void whenGetSize() {
+        assertThat(simpleArrayList.getSize(), is(3));
+    }
 }
