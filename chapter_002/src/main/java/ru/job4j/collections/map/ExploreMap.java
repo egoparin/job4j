278e798d38211a0a5d493a13fdff42d5d88f7812
Oparin Egor <EgOparin@croc.ru>
2019-03-05 10:02:16 +0300

    Map, создать модель User и добавить в map.

---
+++ chapter_002/src/main/java/ru/job4j/collections/map/ExploreMap.java
@@ -0,0 +1,48 @@
+package ru.job4j.collections.map;
+
+import ru.job4j.collections.map.user.User;
+
+import java.time.LocalDate;
+import java.time.Month;
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * Testing map.
+ *
+ * @author Egor Oparin (mailto:egoparin@gmail.com)
+ * @version $Id$
+ * @since 05.03.2019
+ */
+public class ExploreMap {
+
+    private User user1 = new User("Alex", 1, LocalDate.of(1990, Month.AUGUST, 1));
+    private User user2 = new User("Alex", 1, LocalDate.of(1990, Month.AUGUST, 1));
+    private User user3 = new User("Jim", 0, LocalDate.of(1992, Month.JANUARY, 23));
+
+    static Map<User, String> map = new HashMap<>();
+
+    private void testMap() {
+        System.out.println(user1.equals(user2));
+        map.put(user1, "first");
+        map.put(user2, "second");
+        System.out.println(map);
+    }
+
+    /*
+     * Return {ru.job4j.collections.map.user.User@6bc7c054=second,
+     *         ru.job4j.collections.map.user.User@75b84c92=first}
+     *
+     * Для текущей HaspMap в качестве ключа используется класс User, а в качестве значения просто строка.
+     * По умолччанию должно создаться 16 корзин. Через метод put, в Map добавили 2 записи (ключ-значение).
+     * Соответсвенно, т.к ключи разные, то они и находятся в разных корзинах. Распределились по значению hashCode.
+     *  [1*]     [2]     [3*]     [4]     [5]     [6]     [7]     [8]     [9]     [10]    [11]    [12]    [13]    [14]    [15]    [16]
+     *   |       |        |        |       |       |       |       |       |        |       |       |       |       |       |       |
+     *  first           second
+     */
+
+    public static void main (String[] args){
+        ExploreMap exploreMap = new ExploreMap();
+        exploreMap.testMap();
+    }
+}
---
+++ chapter_002/src/main/java/ru/job4j/collections/map/user/User.java
@@ -0,0 +1,50 @@
+package ru.job4j.collections.map.user;
+
+import java.time.LocalDate;
+
+/**
+ * Class user for explore map.
+ *
+ * @author Egor Oparin (mailto:egoparin@gmail.com)
+ * @version $Id$
+ * @since 05.03.2019
+ */
+public class User {
+    private String name;
+    private int children;
+    private LocalDate birthday;
+
+    public User(String name, int children, LocalDate birthday) {
+        this.name = name;
+        this.children = children;
+        this.birthday = birthday;
+    }
+
+    /**
+     * Getters and setters
+     * @return
+     */
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getChildren() {
+        return children;
+    }
+
+    public void setChildren(int children) {
+        this.children = children;
+    }
+
+    public LocalDate getBirthday() {
+        return birthday;
+    }
+
+    public void setBirthday(LocalDate birthday) {
+        this.birthday = birthday;
+    }
+}
